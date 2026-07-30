1bc8ab1446c86bc4d5205c769d32532ca3a1cd73
puja <pghosh09434@gmail.com>
2026-07-30 23:17:39 +0530

    Updated API URL for production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mini-erp-crm-omfy.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
